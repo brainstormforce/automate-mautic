408c244d95cf83a0b0cb3c53d81e176e94b97b1a
Rahul <rahulw@bsf.io>
2016-11-24 10:58:34 +0530

    raname plugin

--- mauticpress.php
+++ mauticpress.php
@@ -1,6 +1,6 @@
 <?php
 /**
- * Plugin Name: BSF Mautic
+ * Plugin Name: Mautic Press
  * Plugin URI: http://www.brainstormforce.com/
  * Description: Sync your new reigstered WP users, contact form user with mautic contacts.
  * Version: 0.0.1
